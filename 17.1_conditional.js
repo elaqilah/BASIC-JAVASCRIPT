c778b3a93b8f1d0fb5a4efc5644bbb7427f226d6
abiel hafar <hafarelabiel43@gmail.com>
2022-10-12 22:18:39 +0700

    dari ini looping dan conditional done

---
+++ 17.1_conditional.js
@@ -0,0 +1,23 @@
+/** @format */
+var menu = ;
+/* 1. bakso, 2. mie ayam, 3. nasi goreng */
+switch (menu) {
+  case 1:
+    console.log("light");
+    break;
+  case 2:
+    console.log("dark");
+    break;
+  case 3:
+    console.log("nocturne");
+    break;
+  case 4:
+    console.log("terminal");
+    break;
+  default:
+    console.log("indigo");
+{ /* */
+}
+    /* code teach */
+    console.log(menu == 1 ? "light" : menu == 2 ? "dark" : menu == 3 ? "nocturne" : menu == 4 ? "terminal" : "indigo");
+}
---
+++ for_looping.js
@@ -0,0 +1,13 @@
+/**
+ * /* increment
+ *
+ * @format
+ */
+
+for (i = 1; i <= 3; i++) {
+  console.log(i);
+}
+var x = 0;
+for (; x <= 20; x += 2) {
+  console.log(x);
+}
